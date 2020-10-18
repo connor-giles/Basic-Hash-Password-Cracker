e8cc2d5e8d8914be36de866fdd6cc22a49708fc3
Connor Giles <connor.giles@ufl.edu>
2020-10-18 16:32:13 -0400

    added comparator

--- part4.py
+++ part4.py
@@ -4,7 +4,7 @@ def md5_100times(passedInHash):
     i = 0
     hash = ""
     print("<==========MD5 Hash==========>")
-    while i < 5:
+    while i < 100:
         if i == 0:
             hash = hashlib.md5(passedInHash.encode())
             print("{} hash: {}".format(i+1, hash.hexdigest()))
@@ -19,7 +19,7 @@ def sha256_100times(passedInHash):
     i = 0
     hash = ""
     print("<==========SHA256 Hash==========>")
-    while i < 5:
+    while i < 100:
         if i == 0:
             hash = hashlib.sha256(passedInHash.encode())
             print("{} hash: {}".format(i+1, hash.hexdigest()))
@@ -34,7 +34,7 @@ def sha512_100times(passedInHash):
     i = 0
     hash = ""
     print("<==========SHA512 Hash==========>")
-    while i < 5:
+    while i < 100:
         if i == 0:
             hash = hashlib.sha512(passedInHash.encode())
             print("{} hash: {}".format(i+1, hash.hexdigest()))
@@ -45,14 +45,20 @@ def sha512_100times(passedInHash):
     print('\n')
     return hash.hexdigest()
 
-# test = open("wordlist5chars.txt", "r")
-# passwordToTry = test.readline()
-# passwordToTry = passwordToTry.rstrip()
+def compareResult(result, compared):
+    if compared == result:
+        print("found password")
+    else:
+        print("did not find password")
 
+file = open("wordlist5chars.txt", "r")
+passwordToTry = file.readline()
+passwordToTry = passwordToTry.rstrip()
+hashToMatch = "122e6946f7641345e4ca3adab598d5a70284bf7e0065621361a868b18bc8f84d958053f0b59f831d8354539867edafc7ec35b8e6cb4cdfaa67843df0d0c42d50"
 
-myHash = md5_100times("aaaaa")
-myHash = sha256_100times(myHash)
-myHash = sha512_100times(myHash)
-# myHash = myHash.decode()
-print("Result: {}".format(myHash))
+firstHash = md5_100times("butler")
+secondHash = sha256_100times(firstHash)
+result = sha512_100times(secondHash)
+compareResult(result, hashToMatch)
+
 
