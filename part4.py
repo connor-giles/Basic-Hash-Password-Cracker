182552aa62fc9e40a3d3d4e7467638c4caa7d951
Connor Giles <connor.giles@ufl.edu>
2020-10-18 17:51:37 -0400

    works just change hash to match

--- part4.py
+++ part4.py
@@ -3,62 +3,65 @@ import hashlib
 def md5_100times(passedInHash):
     i = 0
     hash = ""
-    print("<==========MD5 Hash==========>")
+    #print("<==========MD5 Hash==========>")
     while i < 100:
         if i == 0:
             hash = hashlib.md5(passedInHash.encode())
-            print("{} hash: {}".format(i+1, hash.hexdigest()))
+            #print("{} hash: {}".format(i+1, hash.hexdigest()))
         else:
             hash = hashlib.md5(hash.hexdigest().encode())
-            print("{} hashes: {}".format(i+1, hash.hexdigest()))
+            #print("{} hashes: {}".format(i+1, hash.hexdigest()))
         i += 1
-    print('\n')
+    #print('\n')
     return hash.hexdigest()
 
 def sha256_100times(passedInHash):
     i = 0
     hash = ""
-    print("<==========SHA256 Hash==========>")
+    #print("<==========SHA256 Hash==========>")
     while i < 100:
         if i == 0:
             hash = hashlib.sha256(passedInHash.encode())
-            print("{} hash: {}".format(i+1, hash.hexdigest()))
+            #print("{} hash: {}".format(i+1, hash.hexdigest()))
         else:
             hash = hashlib.sha256(hash.hexdigest().encode())
-            print("{} hashes: {}".format(i+1, hash.hexdigest()))
+            #print("{} hashes: {}".format(i+1, hash.hexdigest()))
         i += 1
-    print('\n')
+    #print('\n')
     return hash.hexdigest()
 
 def sha512_100times(passedInHash):
     i = 0
     hash = ""
-    print("<==========SHA512 Hash==========>")
+    #print("<==========SHA512 Hash==========>")
     while i < 100:
         if i == 0:
             hash = hashlib.sha512(passedInHash.encode())
-            print("{} hash: {}".format(i+1, hash.hexdigest()))
+            #print("{} hash: {}".format(i+1, hash.hexdigest()))
         else:
             hash = hashlib.sha512(hash.hexdigest().encode())
-            print("{} hashes: {}".format(i+1, hash.hexdigest()))
+            #print("{} hashes: {}".format(i+1, hash.hexdigest()))
         i += 1 
-    print('\n')
+    #print('\n')
     return hash.hexdigest()
 
-def compareResult(result, compared):
-    if compared == result:
-        print("found password")
-    else:
-        print("did not find password")
-
 file = open("wordlist5chars.txt", "r")
-passwordToTry = file.readline()
-passwordToTry = passwordToTry.rstrip()
-hashToMatch = "122e6946f7641345e4ca3adab598d5a70284bf7e0065621361a868b18bc8f84d958053f0b59f831d8354539867edafc7ec35b8e6cb4cdfaa67843df0d0c42d50"
+hashToMatch = "00e2033898f86764dd93117e426b76930c5e8ffa3da7b1e095815a1686a011b991dace2783449273552e1c9dbfdbbd9c8aa9d3ecba73b06861fbd88def11e4f0"
 
-firstHash = md5_100times("butler")
-secondHash = sha256_100times(firstHash)
-result = sha512_100times(secondHash)
-compareResult(result, hashToMatch)
+for i in range(pow(26,5)):
+    passwordToTry = file.readline()
+    passwordToTry = passwordToTry.rstrip()
+    temp = passwordToTry
+
+    firstHash = md5_100times(temp)
+    secondHash = sha256_100times(firstHash)
+    result = sha512_100times(secondHash)
+    
+    if result == hashToMatch:
+        print("########## PASSWORD FOUND ########## ==========> {}".format(temp))
+        break
+
+    print("finished: {}".format(temp))
+
 
 
